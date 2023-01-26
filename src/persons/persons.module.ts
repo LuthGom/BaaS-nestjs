bcb65779c58d0315582765628d0ed9df1c25d16a
LuthGom <lucianomendes23@yahoo.com>
2023-01-26 16:54:10 -0300

    add DatabaseModule e personsProviders

--- src/persons/persons.module.ts
+++ src/persons/persons.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PersonsService } from './persons.service';
 import { PersonsController } from './persons.controller';
+import { DatabaseModule } from 'src/shared/database.module';
+import { personsProviders } from './persons.providers';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [PersonsController],
-  providers: [PersonsService]
+  providers: [PersonsService, ...personsProviders],
 })
 export class PersonsModule {}
