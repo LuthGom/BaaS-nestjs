2e670c49a12a92e18bcc203256f0ff99934a658f
LuthGom <lucianomendes23@yahoo.com>
2023-01-30 19:43:35 -0300

    passando index unic true para account, email e cpf

--- src/schemas/person.schema.ts
+++ src/schemas/person.schema.ts
@@ -5,14 +5,18 @@ mongoose.Promise;
 
 const PersonSchema = new mongoose.Schema<Person>({
   name: { type: String, required: true },
-  cpf: { type: String, required: true, index:{unique: true} },
+  cpf: { type: String, required: true, index: { unique: true } },
   address: { type: Object, required: true },
   cellphone: { type: String, required: true },
-  email: { type: String, required: true, index:{unique: true} },
-  account: { type: Number, default: geradorDeContas(), index:{unique: true} },
+  email: { type: String, required: true, index: { unique: true } },
+  account: {
+    type: Number,
+    default: geradorDeContas(),
+    index: { unique: true },
+  },
   vd: { type: Number, default: 1 },
   saldo: { type: Number, default: 0 },
-  password: String,
+  password: { type: String, required: true },
   createdAt: Date,
 });
 
