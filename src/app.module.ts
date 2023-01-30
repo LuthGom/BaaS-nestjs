2f8ff75a63c171df9e32de4a42471bfcb2c9b9d1
LuthGom <lucianomendes23@yahoo.com>
2023-01-30 19:41:29 -0300

    importand authModule no appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PersonsModule } from './persons/persons.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PersonsModule],
+  imports: [ConfigModule.forRoot(), PersonsModule, AuthModule],
 })
 export class AppModule {}
