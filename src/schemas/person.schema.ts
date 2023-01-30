e5d0e7010ae903afc1a3a7a5bae0d14fa768b13f
LuthGom <lucianomendes23@yahoo.com>
2023-01-30 15:38:29 -0300

    passando prop unique para cpf, email e account no PersonSchema

--- src/schemas/person.schema.ts
+++ src/schemas/person.schema.ts
@@ -5,11 +5,11 @@ mongoose.Promise;
 
 const PersonSchema = new mongoose.Schema<Person>({
   name: { type: String, required: true },
-  cpf: { type: String, required: true },
+  cpf: { type: String, required: true, index:{unique: true} },
   address: { type: Object, required: true },
   cellphone: { type: String, required: true },
-  email: { type: String, required: true },
-  account: { type: Number, default: geradorDeContas() },
+  email: { type: String, required: true, index:{unique: true} },
+  account: { type: Number, default: geradorDeContas(), index:{unique: true} },
   vd: { type: Number, default: 1 },
   saldo: { type: Number, default: 0 },
   password: String,
