7da15c35ccd66b326204f1e268b50372ad827ed3
LuthGom <lucianomendes23@yahoo.com>
2023-01-31 14:54:19 -0300

    add AuthService e JwtService como providers

--- src/persons/persons.module.ts
+++ src/persons/persons.module.ts
@@ -3,11 +3,12 @@ import { PersonsService } from './persons.service';
 import { PersonsController } from './persons.controller';
 import { DatabaseModule } from 'src/shared/database.module';
 import { personsProviders } from './persons.providers';
-
+import { AuthService } from 'src/auth/auth.service';
+import { JwtService } from '@nestjs/jwt';
 @Module({
   imports: [DatabaseModule],
   controllers: [PersonsController],
-  providers: [PersonsService, ...personsProviders],
+  providers: [PersonsService,AuthService,JwtService,...personsProviders],
   exports: [PersonsService],
 })
 export class PersonsModule {}
