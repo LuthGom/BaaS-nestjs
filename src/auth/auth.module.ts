c786b960b62cb26969b17ac9099cb92bef92d1dd
LuthGom <lucianomendes23@yahoo.com>
2023-01-31 10:47:20 -0300

    importando JwtModule com signOptions

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,17 @@ import { AuthService } from './auth.service';
 import { PersonsModule } from 'src/persons/persons.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
-
+import { JwtModule, JwtSecretRequestType } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
 @Module({
-  imports: [PersonsModule, PassportModule],
+  imports: [
+    PersonsModule,
+    PassportModule,
+    JwtModule.register({
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   providers: [AuthService, LocalStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
