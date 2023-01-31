b892eee09241bfb7bca0c1536eba44a1e6d5dfd3
LuthGom <lucianomendes23@yahoo.com>
2023-01-31 14:57:10 -0300

    mudando validação para compare de senha

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable } from '@nestjs/common';
 import { PersonsService } from 'src/persons/persons.service';
 import { JwtService } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
+import { compare } from 'bcrypt';
 @Injectable()
 export class AuthService {
   constructor(
@@ -11,7 +12,8 @@ export class AuthService {
 
   async validatePerson(email: string, pass?: string): Promise<any> {
     const person = await this.personsService.findOne(email);
-    if (person && person[0].password === pass) {
+
+    if (person && compare(pass, person[0].password)) {
       const { password, ...result } = person[0];
       return result;
     }
