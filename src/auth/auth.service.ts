89a89a2f465d8dbc078d2f5aa36d2ebfb4711fda
LuthGom <lucianomendes23@yahoo.com>
2023-01-31 10:47:38 -0300

    importando JwtService e criando metodo login com token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { PersonsService } from 'src/persons/persons.service';
-
+import { JwtService } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
 @Injectable()
 export class AuthService {
-  constructor(private personsService: PersonsService) {}
+  constructor(
+    private personsService: PersonsService,
+    private jwtService: JwtService,
+  ) {}
 
   async validatePerson(email: string, pass?: string): Promise<any> {
     const person = await this.personsService.findOne(email);
@@ -12,5 +16,11 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+  async login(user) {
+    const payload = { username: user.email, sub: user._id };
+    return {
+      token: this.jwtService.sign(payload, { secret: jwtConstants.secret }),
+    };
   }
 }
