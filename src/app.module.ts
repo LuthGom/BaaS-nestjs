4ae650373b1b292b43310624792342690e91ba39
LuthGom <lucianomendes23@yahoo.com>
2023-02-04 23:06:43 -0300

    criando estrutura para entidade accounts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PersonsModule } from './persons/persons.module';
 import { AuthModule } from './auth/auth.module';
+import { AccountsModule } from './accounts/accounts.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PersonsModule, AuthModule],
+  imports: [ConfigModule.forRoot(), PersonsModule, AuthModule, AccountsModule],
 })
 export class AppModule {}
