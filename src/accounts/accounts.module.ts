d4f5d62114a6b0b554c91787401e30d3401891bb
LuthGom <lucianomendes23@yahoo.com>
2023-02-06 22:58:56 -0300

    add databsemodule e accountsservices

--- src/accounts/accounts.module.ts
+++ src/accounts/accounts.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AccountsService } from './accounts.service';
 import { AccountsController } from './accounts.controller';
+import { DatabaseModule } from 'src/shared/database.module';
+import { personsProviders } from 'src/persons/persons.providers';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [AccountsController],
-  providers: [AccountsService]
+  providers: [AccountsService, ...personsProviders],
+  exports: [AccountsService],
 })
 export class AccountsModule {}
